2cc707e4113307dfa11f0587f91b6af82d894a56
Egor Kurunov <eegor2257@gmail.com>
2026-07-23 12:29:15 +0300

    Доработал кодстайл

--- src/main/resources/db.changelog/dbscripts/26_07_06_v1_create_table_currencies.sql
+++ src/main/resources/db.changelog/dbscripts/26_07_06_v1_create_table_currencies.sql
@@ -1,6 +1,6 @@
 CREATE TABLE currencies
 (
-    id     UUID PRIMARY KEY,
-    code   CHAR(3)     NOT NULL CHECK (TRIM(code) <> ''),
-    name   VARCHAR(50) NOT NULL CHECK (TRIM(name) <> ''),
+    id   UUID PRIMARY KEY,
+    code CHAR(3)     NOT NULL CHECK (TRIM(code) <> ''),
+    name VARCHAR(50) NOT NULL CHECK (TRIM(name) <> ''),
 );
--- src/main/resources/db.changelog/dbscripts/26_07_06_v1_create_table_transactions.sql
+++ src/main/resources/db.changelog/dbscripts/26_07_06_v1_create_table_transactions.sql
@@ -1,13 +1,13 @@
 CREATE TABLE transactions
 (
-    id          UUID PRIMARY KEY,
-    user_id     UUID                     NOT NULL,
-    category_id UUID                     NOT NULL,
-    currency_id UUID                     NOT NULL,
-    amount      DECIMAL(15, 2) DEFAULT 0.00,
-    bank        VARCHAR(50),
-    description VARCHAR(255),
-    created_date  DATE,
+    id           UUID PRIMARY KEY,
+    user_id      UUID NOT NULL,
+    category_id  UUID NOT NULL,
+    currency_id  UUID NOT NULL,
+    amount       DECIMAL(15, 2) DEFAULT 0.00,
+    bank         VARCHAR(50),
+    description  VARCHAR(255),
+    created_date DATE,
 
     FOREIGN KEY (category_id) REFERENCES categories (id),
     FOREIGN KEY (currency_id) REFERENCES currencies (id),
