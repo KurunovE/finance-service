aa8a8d785369b36ec9eaa1dc7296447faff16932
Egor Kurunov <eegor2257@gmail.com>
2026-08-29 15:57:06 +0300

    Добавил сервисный слой для валют

--- src/main/java/com/prorenta/financeservice/service/impl/CurrencyServiceImpl.java
+++ src/main/java/com/prorenta/financeservice/service/impl/CurrencyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.prorenta.financeservice.service.impl;
 
 import com.prorenta.financeservice.exception.CurrencyNotFoundException;
+import com.prorenta.financeservice.mapper.CurrencyMapper;
 import com.prorenta.financeservice.model.dto.CurrencyResponseDto;
 import com.prorenta.financeservice.model.dto.ListCurrenciesResponseDto;
 import com.prorenta.financeservice.model.entity.Currency;
@@ -11,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
@@ -19,6 +21,7 @@ import java.util.UUID;
 public class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrencyRepository currencyRepository;
+    private final CurrencyMapper currencyMapper;
 
     @Override
     @Transactional(readOnly = true)
@@ -31,12 +34,24 @@ public class CurrencyServiceImpl implements CurrencyService {
     @Override
     @Transactional(readOnly = true)
     public CurrencyResponseDto getCurrency(UUID currencyId) {
-        return null;
+        log.info("Получение валюты: currencyId={}", currencyId);
+        Currency currency = findById(currencyId);
+        log.info("Валюта успешно получена: Currency={}", currency);
+        return currencyMapper.mapCurrencyToCurrencyResponseDto(currency);
     }
 
     @Override
     @Transactional(readOnly = true)
     public ListCurrenciesResponseDto getCurrencies() {
-        return null;
+        log.info("Получение списка всех валют");
+        List<Currency> currencyList = currencyRepository.findAll();
+        log.info("Список валют получен: size={}", currencyList.size());
+        return ListCurrenciesResponseDto.builder()
+                .currencies(
+                        currencyList.stream()
+                                .map(currencyMapper::mapCurrencyToCurrencyResponseDto)
+                                .toList()
+                )
+                .build();
     }
 }
