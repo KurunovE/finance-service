54fdcb81a862f506ca8eda76d3b5e61d3213e78f
Egor Kurunov <eegor2257@gmail.com>
2026-08-29 13:14:15 +0300

    Исправил нейминг записи

--- src/main/java/com/prorenta/financeservice/model/dto/ListCurrenciesResponseDto.java
+++ src/main/java/com/prorenta/financeservice/model/dto/ListCurrenciesResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import java.util.List;
 
 @Builder
-public record ListCurrencyResponseDto(
+public record ListCurrenciesResponseDto(
         List<CurrencyResponseDto> currency
 ) {
 }
