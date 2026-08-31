d81a5d74d5371a00fcaea818443476a504e26b81
Egor Kurunov <eegor2257@gmail.com>
2026-08-31 15:08:42 +0300

    Добавил скрипты бд для реализации интеграционных тестов

---
+++ src/test/resources/sql/cleanup.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE
+    transactions, categories, currencies
+CASCADE;
---
+++ src/test/resources/sql/insert_currency.sql
@@ -0,0 +1,2 @@
+INSERT INTO currencies (id, code, name, symbol, is_active)
+VALUES ('33333333-3333-3333-3333-333333333333', 'RUB', 'Российский рубль', '₽', true);
