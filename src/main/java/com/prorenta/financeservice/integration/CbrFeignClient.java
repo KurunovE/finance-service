d491acb1e47b803e526bb0f6e3b657a2cd9028c2
Egor Kurunov <eegor2257@gmail.com>
2026-09-01 20:58:41 +0300

    Параметризирровал url для запросов

--- src/main/java/com/prorenta/financeservice/integration/CbrFeignClient.java
+++ src/main/java/com/prorenta/financeservice/integration/CbrFeignClient.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "cbr-client",
-        url = "https://www.cbr.ru/scripts",
+        url = "${cbr.exchange-rates.url}",
         configuration = CbrFeignConfig.class
 )
 public interface CbrFeignClient {
 
-    @GetMapping(value = "/XML_daily.asp", consumes = "application/xml")
+    @GetMapping(value = "${cbr.exchange-rates.value}", consumes = "application/xml")
     ValCursResponseDto getDailyRates(
             @RequestParam(value = "date_req", required = false) String dateReq
     );
