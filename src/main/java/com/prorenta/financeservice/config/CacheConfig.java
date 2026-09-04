a0e7c3fb9880696984a9e0942daf8cb0d5a79f54
Egor Kurunov <eegor2257@gmail.com>
2026-09-04 18:21:26 +0300

    Добавил конфиг и тесты кэширования

---
+++ src/main/java/com/prorenta/financeservice/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.prorenta.financeservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
